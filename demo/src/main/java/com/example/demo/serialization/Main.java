05f19dc2f6f557910202c469b5155a908186bfdf
OMAR <ojramirezv@gmail.com>
2022-06-02 02:01:32 -0500

    adding serialization example

--- demo/src/main/java/com/example/demo/serialization/Main.java
+++ demo/src/main/java/com/example/demo/serialization/Main.java
@@ -1,9 +1,9 @@
 package com.example.demo.serialization;
 
 import java.io.*;
-
 public class Main {
-    public static void main(String[] args){
+    //This static method allows to serializate the information of a class into a byte format inside a file.
+    /*public static void main(String[] args) throws IOException{
         User user = new User();
         user.name = "Bro";
         user.password = "I<pizza";
@@ -12,5 +12,16 @@ public class Main {
         out.writeObject(user);
         out.close();
         fileOutputStream.close();
+        System.out.println("object info saved! :");
+    }*/
+    //This static method allows to de-serializate the information from yte format inside a file into a class format.
+    public static void main(String[] args) throws  IOException,ClassNotFoundException{
+        User user = null;
+        FileInputStream fileInputStream = new FileInputStream("/home/omar/Documents/workspace/demo/UserInfo.ser");
+        ObjectInputStream in = new ObjectInputStream(fileInputStream);
+        user = (User) in.readObject();
+        in.close();
+        fileInputStream.close();
+        System.out.println(user.name+" "+user.password);
     }
 }
