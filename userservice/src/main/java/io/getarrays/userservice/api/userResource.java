7e25c5269fe1bb72879ec838e37ec6f66e9c49bb
OMAR <ojramirezv@gmail.com>
2022-06-26 16:06:59 -0500

    creation of tables are already usefull

--- userservice/src/main/java/io/getarrays/userservice/api/userResource.java
+++ userservice/src/main/java/io/getarrays/userservice/api/userResource.java
@@ -4,6 +4,7 @@ import io.getarrays.userservice.domain.User;
 import io.getarrays.userservice.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,7 +14,9 @@ import java.util.List;
 @RequiredArgsConstructor
 public class userResource {
     private final UserService userService;
+    @GetMapping("/users")
     public ResponseEntity<List<User>> getUsers(){
+
         return ResponseEntity.ok().body(userService.getUsers());
     }
 }
