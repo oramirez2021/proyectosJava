891d4e8dc087c9d9568e8512341c09ed6e722be7
OMAR <ojramirezv@gmail.com>
2022-06-27 08:13:15 -0500

    adding new security package

---
+++ userservice/src/main/java/io/getarrays/userservice/security/SecurityConfig.java
@@ -0,0 +1,6 @@
+package io.getarrays.userservice.security;
+
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+}
