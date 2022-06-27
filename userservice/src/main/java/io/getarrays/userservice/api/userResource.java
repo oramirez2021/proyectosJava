cbd11732e616dcf54e42d70d134cbbb047041a8d
OMAR <ojramirezv@gmail.com>
2022-06-27 01:25:30 -0500

    adding cration in runtime users, roles, and adding roles to users

--- userservice/src/main/java/io/getarrays/userservice/api/userResource.java
+++ userservice/src/main/java/io/getarrays/userservice/api/userResource.java
@@ -1,13 +1,15 @@
 package io.getarrays.userservice.api;
 
+import io.getarrays.userservice.domain.Role;
 import io.getarrays.userservice.domain.User;
 import io.getarrays.userservice.service.UserService;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController @RequestMapping("/api")
@@ -19,4 +21,25 @@ public class userResource {
 
         return ResponseEntity.ok().body(userService.getUsers());
     }
+    @PostMapping("/user/save")
+    public ResponseEntity<User> saveUser(@RequestBody User user){
+        URI uri =  URI.create(ServletUriComponentsBuilder.fromCurrentContextPath().path("api/user/save").toUriString());
+        return ResponseEntity.created(uri).body(userService.saveUser(user));
+    }
+    @PostMapping("/role/save")
+    public ResponseEntity<Role> saveRol(@RequestBody Role role){
+        URI uri =  URI.create(ServletUriComponentsBuilder.fromCurrentContextPath().path("api/role/save").toUriString());
+        return ResponseEntity.created(uri).body(userService.saveRole(role));
+    }
+    @PostMapping("/role/addtouser")
+    public ResponseEntity<?> addRoleToUser(@RequestBody RoleToUserForm form){
+        userService.addRoleToUser(form.getUserName(),form.getRoleName());
+        return ResponseEntity.ok().build();
+    }
+
+}
+@Data
+class RoleToUserForm{
+    private String userName;
+    private String roleName;
 }
