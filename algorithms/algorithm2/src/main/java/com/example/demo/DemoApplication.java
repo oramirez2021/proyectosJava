24e740f07c137dadf2b401d8f9c759c57c4531bf
OMAR <ojramirezv@gmail.com>
2022-09-08 21:32:31 -0500

    summarizing valus with algorithm and mutatin into a new array with the same length of the base array

---
+++ algorithms/algorithm2/src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,41 @@
+package com.example.demo;
+
+import edu.princeton.cs.algs4.*;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		int a[] = {3,2,7,-2};
+		int n = a.length;
+		int b[] = new int[n];
+		int aa = 0;
+		int bb = 0;
+		int cc = 0;
+		for (int i = 0; i < n; i++) {
+			try{
+				aa = a[i-1];
+			}catch(Exception e){
+				aa = 0;
+				e.toString();
+			}
+			try{
+				bb = a[i];
+			}catch(Exception e){
+				bb = 0;
+				e.toString();
+			}
+			try{
+				cc = a[i+1];
+			}catch(Exception e){
+				cc = 0;
+				e.toString();
+			}
+			b[i] = aa + bb + cc;
+			System.out.println(b[i]);
+		}
+
+	}
+
+}
