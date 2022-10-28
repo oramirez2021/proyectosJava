311a16795f9afc8d456f78b9b5503e733e5187c4
OMAR <ojramirezv@gmail.com>
2022-10-28 03:20:28 -0500

    adding a new solution to solve the algorithm

--- algorithms/algorithm1/src/main/java/com/example/demo/DemoApplication.java
+++ algorithms/algorithm1/src/main/java/com/example/demo/DemoApplication.java
@@ -1,14 +1,16 @@
 package com.example.demo;
 
-import edu.princeton.cs.algs4.*;
+//import edu.princeton.cs.algs4.*;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Scanner;
 
 
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		//StdOut.println(StdIn.readString());
-		String pattern = args[0];
+		/*String pattern = args[0];
 		String pattern_splited[] = new String[pattern.length()];
 		pattern_splited = pattern.split("");
 		String source = args[1];
@@ -42,8 +44,51 @@ public class DemoApplication {
 				break;
 			}
 		}
-		System.out.println("match "+match);
-
+		System.out.println("match "+match);*/
+		//SOLUTION 2//
+		Scanner scn = new Scanner(System.in);
+		System.out.println("Enter a text");
+		String text= scn.next();
+		String number = scn.next();
+		int condition1 = 0;
+		int condition2 = 0;
+		int condition3 = 0;
+		int numberVowels = 0;
+		int numberConsonants = 0;
+		int numberCeros = 0;
+		int numberOnes = 0;
+		int match = 0;
+		String vowels = "aeiouy";
+		//Analysing condition 1
+		if (text.length() == number.length()){
+			condition1 = 1;
+		}
+		for (String x : text.split("")) {
+			if (vowels.contains(x)){
+				numberVowels+=1;
+			}else{
+				numberConsonants+=1;
+			}
+		}
+		for (String y : number.split("")) {
+			if (0 == Integer.parseInt(y)){
+				numberCeros+=1;
+			}else{
+				numberOnes+=1;
+			}
+		}
+		//Analysing condition 2
+		if(numberVowels == numberCeros){
+			condition2 = 1;
+		}
+		//Analysing condition 3
+		if(numberConsonants == numberOnes){
+			condition3 = 1;
+		}
+		if(condition1 == 1 && condition2 == 1 && condition3 == 1){
+			match = 1;
+		}
+		System.out.println("match :"+match);
 	}
 
 }
