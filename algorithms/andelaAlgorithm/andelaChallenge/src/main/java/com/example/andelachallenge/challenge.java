e903824437849aad74399b7bfd0e192d9ecfde6e
omar <ojramirezv@gmail.com>
2022-11-11 03:23:38 -0500

    another way to solve the problem

--- algorithms/andelaAlgorithm/andelaChallenge/src/main/java/com/example/andelachallenge/challenge.java
+++ algorithms/andelaAlgorithm/andelaChallenge/src/main/java/com/example/andelachallenge/challenge.java
@@ -1,8 +1,6 @@
 package com.example.andelachallenge;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
+import java.util.*;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
@@ -11,36 +9,32 @@ class Main{
         System.out.println("Hello LeetCoder");
         String banned[]= {"hit"};
         Interview interview = new Interview();
-        System.out.println(interview.mostCommonWord("Bob hit a ball, the hit BALL flew far after it was hit",banned));
+        Scanner scanner =  new Scanner(System.in);
+        //paragraph: Bob hit a ball, the hit BALL flew far after it was hit.
+        System.out.println(interview.mostCommonWord(scanner.nextLine(),banned));
     }
 }
 class Interview{
 
     public String mostCommonWord(String paragraph, String[] banned) {
         String ban = banned[0];
-        paragraph = paragraph.replace(" "+ban,"");
-        paragraph = paragraph.replace(",","");
-        paragraph = paragraph.replace(".","").toUpperCase();
-        String[] arr_para = paragraph.split(" ");
-        ArrayList<String> arrayList =  new ArrayList<>();
-        for(String word :  arr_para){
-            arrayList.add(word);
-        }
+        paragraph = paragraph.replace(" "+ban,"").replace(",","").replace(".","").toUpperCase();
+        List<String> arrayList = Arrays.asList(paragraph.split(" "));
         HashMap<String, Integer> hashMap = new HashMap<>();
         Predicate<String> predicate = null;
-        List<String> list = null;
+        Integer contador = 0;
+        int major = 0;
         for(String word: arrayList){
             predicate = text -> text.equals(word);
-            list =  arrayList.stream().filter(predicate).collect(Collectors.toList());
-            hashMap.put(word,list.size());
-        }
-        int major = 0;
-        for(Integer value : hashMap.values()){
-            if(value>major){
-                major = value;
+            contador = arrayList.stream().filter(predicate).collect(Collectors.toList()).size();
+            hashMap.put(word,contador);
+            //calculatin the major
+            if (contador>major){
+                major = contador;
             }
         }
-
         return ""+major;
     }
+
+
 }
