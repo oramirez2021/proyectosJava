ec90ca414d416fdabf1d66ff2aeb5b9a29347de3
omar <ojramirezv@gmail.com>
2022-11-16 05:37:35 -0500

    new solution v2

--- algorithms/devlane/src/main/java/com/example/devlane/Counter.java
+++ algorithms/devlane/src/main/java/com/example/devlane/Counter.java
@@ -7,7 +7,7 @@ public class Counter {
     public static void main(String[] args){
         Scanner scanner =  new Scanner(System.in);
         String text = scanner.nextLine();
-        text = text+"0";
+        text = text;
         Interview interview = new Interview();
         System.out.println(interview.cuentaLetras(text));
     }
@@ -18,28 +18,27 @@ public class Counter {
 class Interview{
     public String cuentaLetras(String text) {
         String array[] = text.split("");
-        int contador = 0;
-        String eval = "";
-        eval = array[0];
-        ArrayList<String> arrayList = new ArrayList<>();
-        int cont = 1;
-        String cadenaFinal = "";
-        for (int i = 1; i < array.length; i++) {
-            if(eval.equals(array[i])){
-                cont+=1;
-            }else{
-                arrayList.add(eval);
-                arrayList.add(""+cont);
-                cadenaFinal = cadenaFinal+eval;
-                cadenaFinal = cadenaFinal+cont;
-                cont=1;
-                eval=array[i];
-                if(array[i].equals("0")){
+        Integer len = array.length;
+        int cont = 0;
+        System.out.println(text.indexOf(array[0],2));
+        String chr = "";
+        String newString = "";
+        int pos = 0;
+        int i = pos;
+        while (i < len){
+            chr = array[i];
+            for(pos = i;pos<=len;pos++){
+                if(text.indexOf(chr,pos)==pos){
+                    cont++;
+                }else{
+                    newString = newString + chr + cont;
+                    cont = 0;
+                    i = pos;
                     break;
                 }
             }
-
         }
-        return cadenaFinal;
+
+        return newString;
     }
 }
